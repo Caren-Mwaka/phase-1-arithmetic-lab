e74dc28c4500ff6f4863bc2128d211987f325987
Caren Wanjiru Mwaka Chibwara <carrienjiru40@gmail.com>
2024-03-20 16:04:04 +0300

    modifies index.js

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
-// Write your code here
+const num1 = 62
+const num2 = 1
+let multiply = num1*num2; 
+console.log (multiply);
+let random = Math.floor(Math.random() * 100) + 1; 
+console.log(random);
+const num3 = 48
+const num4 = 12
+let mod = num3/num4;
+console.log(mod);
+let max = Math.max(15, 16, 17 ,18 ,19, 20);
+console.log(max);
+  
